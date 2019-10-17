50fb257839545a41eecefaff4b7ccb3919a39a79
Azat Ibrakov <azatibrakov@gmail.com>
2019-10-17 21:27:33 +0300

    Add test for 'Tree.discard' method

---
+++ tests/binary_tests/tree_tests/test_discard.py
@@ -0,0 +1,35 @@
+from copy import deepcopy
+from typing import Tuple
+
+from hypothesis import given
+
+from dendroid.binary import Tree
+from dendroid.hints import Domain
+from . import strategies
+
+
+@given(strategies.empty_trees, strategies.totally_ordered_values)
+def test_basic(tree: Tree, value: Domain) -> None:
+    result = tree.discard(value)
+
+    assert result is None
+
+
+@given(strategies.empty_trees, strategies.totally_ordered_values)
+def test_base_case(tree: Tree, value: Domain) -> None:
+    tree.discard(value)
+
+    assert not tree
+    assert len(tree) == 0
+    assert value not in tree
+
+
+@given(strategies.trees_with_totally_ordered_values)
+def test_step(tree_with_value: Tuple[Tree, Domain]) -> None:
+    tree, value = tree_with_value
+    original = deepcopy(tree)
+
+    tree.discard(value)
+
+    assert len(tree) + (value in original) == len(original)
+    assert value not in tree
