8f4efaf1b83b190d8c9e341fa89d04e801eff88d
Azat Ibrakov <azatibrakov@gmail.com>
2019-10-18 16:20:29 +0300

    Add test for 'Tree.__lt__' magic method

---
+++ tests/binary_tests/tree_tests/test_lower_than.py
@@ -0,0 +1,35 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from dendroid.binary import Tree
+from tests.utils import (equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.trees)
+def test_irreflexivity(tree: Tree) -> None:
+    assert not tree < tree
+
+
+@given(strategies.trees_pairs)
+def test_asymmetry(trees_pair: Tuple[Tree, Tree]) -> None:
+    first_tree, second_tree = trees_pair
+
+    assert implication(first_tree < second_tree, not second_tree < first_tree)
+
+
+@given(strategies.trees_triplets)
+def test_transitivity(trees_triplet: Tuple[Tree, Tree, Tree]) -> None:
+    first_tree, second_tree, third_tree = trees_triplet
+
+    assert implication(first_tree < second_tree < third_tree,
+                       first_tree < third_tree)
+
+
+@given(strategies.trees_pairs)
+def test_relation_with_greater_than(trees_pair: Tuple[Tree, Tree]) -> None:
+    first_tree, second_tree = trees_pair
+
+    assert equivalence(first_tree < second_tree, second_tree > first_tree)
