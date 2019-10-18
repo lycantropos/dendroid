a386ada65225091e4d14497b0c1c6f7b306a0215
Azat Ibrakov <azatibrakov@gmail.com>
2019-10-18 16:15:12 +0300

    Add test for 'Tree.__ge__' magic method

---
+++ tests/binary_tests/tree_tests/test_greater_than_or_equals.py
@@ -0,0 +1,44 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from dendroid.binary import Tree
+from tests.utils import (equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.trees)
+def test_reflexivity(tree: Tree) -> None:
+    assert tree >= tree
+
+
+@given(strategies.trees_pairs)
+def test_antisymmetry(trees_pair: Tuple[Tree, Tree]) -> None:
+    first_tree, second_tree = trees_pair
+
+    assert equivalence(first_tree >= second_tree >= first_tree,
+                       first_tree == second_tree)
+
+
+@given(strategies.trees_triplets)
+def test_transitivity(trees_triplet: Tuple[Tree, Tree, Tree]) -> None:
+    first_tree, second_tree, third_tree = trees_triplet
+
+    assert implication(first_tree >= second_tree >= third_tree,
+                       first_tree >= third_tree)
+
+
+@given(strategies.trees_pairs)
+def test_relation_with_greater_than(trees_pair: Tuple[Tree, Tree]) -> None:
+    first_tree, second_tree = trees_pair
+
+    assert implication(first_tree > second_tree, first_tree >= second_tree)
+
+
+@given(strategies.trees_pairs)
+def test_relation_with_lower_than_or_equals(trees_pair: Tuple[Tree, Tree]
+                                            ) -> None:
+    first_tree, second_tree = trees_pair
+
+    assert equivalence(first_tree >= second_tree, second_tree <= first_tree)
