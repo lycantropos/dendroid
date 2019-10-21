71190ea09b00f0e9b5f621f92e334e7f8e77efd5
Azat Ibrakov <azatibrakov@gmail.com>
2019-10-21 17:57:41 +0300

    Add test for 'Tree.__and__' magic method

---
+++ tests/binary_tests/tree_tests/test_intersection.py
@@ -0,0 +1,83 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from dendroid.binary import Tree
+from . import strategies
+
+
+@given(strategies.trees)
+def test_idempotence(tree: Tree) -> None:
+    result = tree & tree
+
+    assert result == tree
+
+
+@given(strategies.empty_trees, strategies.trees)
+def test_left_absorbing_element(empty_tree: Tree, tree: Tree) -> None:
+    result = empty_tree & tree
+
+    assert not result
+
+
+@given(strategies.empty_trees, strategies.trees)
+def test_right_absorbing_element(empty_tree: Tree, tree: Tree) -> None:
+    result = tree & empty_tree
+
+    assert not result
+
+
+@given(strategies.trees_pairs)
+def test_absorption_identity(trees_pair: Tuple[Tree, Tree]) -> None:
+    left_tree, right_tree = trees_pair
+
+    result = left_tree & (left_tree | right_tree)
+
+    assert result == left_tree
+
+
+@given(strategies.trees_pairs)
+def test_commutativity(trees_pair: Tuple[Tree, Tree]) -> None:
+    left_tree, right_tree = trees_pair
+
+    result = left_tree & right_tree
+
+    assert result == right_tree & left_tree
+
+
+@given(strategies.trees_triplets)
+def test_associativity(trees_triplet: Tuple[Tree, Tree, Tree]) -> None:
+    left_tree, mid_tree, right_tree = trees_triplet
+
+    result = (left_tree & mid_tree) & right_tree
+
+    assert result == left_tree & (mid_tree & right_tree)
+
+
+@given(strategies.trees_triplets)
+def test_difference_operand(trees_triplet: Tuple[Tree, Tree, Tree]) -> None:
+    left_tree, mid_tree, right_tree = trees_triplet
+
+    result = (left_tree - mid_tree) & right_tree
+
+    assert result == (left_tree & right_tree) - mid_tree
+
+
+@given(strategies.trees_triplets)
+def test_distribution_over_union(trees_triplet: Tuple[Tree, Tree, Tree]
+                                 ) -> None:
+    left_tree, mid_tree, right_tree = trees_triplet
+
+    result = left_tree & (mid_tree | right_tree)
+
+    assert result == (left_tree & mid_tree) | (left_tree & right_tree)
+
+
+@given(strategies.trees_pairs)
+def test_connection_with_subset_relation(trees_pair: Tuple[Tree, Tree]
+                                         ) -> None:
+    left_tree, right_tree = trees_pair
+
+    result = left_tree & right_tree
+
+    assert result <= left_tree and result <= right_tree
