3f766fb15f0d405b900aec9e366b8d936a0cd107
Ibrakov Azat <azatibrakov@gmail.com>
2019-10-27 00:28:06 +0300

    Complete strategies

--- tests/strategies/literals.py
+++ tests/strategies/literals.py
@@ -1,13 +1,17 @@
 from functools import partial
 
 from hypothesis import strategies
-from lz.functional import identity
+from lz.functional import (identity,
+                           pack)
 
 totally_ordered_values_strategies = (
     strategies.sampled_from([strategies.integers(),
                              strategies.floats(allow_nan=False),
                              strategies.booleans(),
                              strategies.text()]))
+totally_ordered_values_strategies |= (
+    strategies.lists(totally_ordered_values_strategies,
+                     max_size=100).map(pack(strategies.tuples)))
 totally_ordered_values = totally_ordered_values_strategies.flatmap(identity)
 totally_ordered_values_lists = (totally_ordered_values_strategies
                                 .flatmap(strategies.lists))
