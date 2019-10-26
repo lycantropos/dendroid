367f69af30bd1979064669d3994ab2f725845996
Ibrakov Azat <azatibrakov@gmail.com>
2019-10-26 09:15:19 +0300

    Complete test for 'Tree.remove' method

--- tests/binary_tests/tree_tests/test_remove.py
+++ tests/binary_tests/tree_tests/test_remove.py
@@ -1,5 +1,7 @@
 import pytest
 from hypothesis import given
+from lz.iterating import (first,
+                          last)
 
 from dendroid.binary import Tree
 from dendroid.hints import Domain
@@ -7,6 +9,24 @@ from . import strategies
 
 
 @given(strategies.empty_trees, strategies.totally_ordered_values)
-def test_empty_tree(tree: Tree, value: Domain) -> None:
+def test_base_case(tree: Tree, value: Domain) -> None:
     with pytest.raises(KeyError):
         tree.remove(value)
+
+
+@given(strategies.non_empty_trees)
+def test_step_left(tree: Tree) -> None:
+    value = first(tree)
+
+    tree.remove(value)
+
+    assert value not in tree
+
+
+@given(strategies.non_empty_trees)
+def test_step_right(tree: Tree) -> None:
+    value = last(tree)
+
+    tree.remove(value)
+
+    assert value not in tree
