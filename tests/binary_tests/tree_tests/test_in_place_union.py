38ba17041c100b1d4186594c9860df8c91ef479f
Ibrakov Azat <azatibrakov@gmail.com>
2019-10-26 09:02:58 +0300

    Add test for 'Tree.__ior__' magic method

---
+++ tests/binary_tests/tree_tests/test_in_place_union.py
@@ -0,0 +1,17 @@
+from copy import deepcopy
+from typing import Tuple
+
+from hypothesis import given
+
+from dendroid.binary import Tree
+from . import strategies
+
+
+@given(strategies.trees_pairs)
+def test_connection_with_intersection(trees_pair: Tuple[Tree, Tree]) -> None:
+    left_tree, right_tree = trees_pair
+    original_left_tree = deepcopy(left_tree)
+
+    left_tree |= right_tree
+
+    assert left_tree == original_left_tree | right_tree
