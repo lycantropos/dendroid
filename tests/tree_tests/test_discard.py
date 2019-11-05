d9908029f14bc42da33933d8defcdb60e1a0907b
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-05 19:04:34 +0300

    Complete test for 'TreeBase.discard' method

--- tests/tree_tests/test_discard.py
+++ tests/tree_tests/test_discard.py
@@ -4,15 +4,28 @@ from typing import Tuple
 from hypothesis import given
 
 from dendroid.hints import Domain
-from tests.utils import Tree
+from tests.utils import (Tree,
+                         is_left_subtree_less_than_right_subtree)
 from . import strategies
 
 
-@given(strategies.empty_trees, strategies.totally_ordered_values)
-def test_basic(tree: Tree, value: Domain) -> None:
+@given(strategies.trees_with_totally_ordered_values)
+def test_basic(tree_with_value: Tuple[Tree, Domain]) -> None:
+    tree, value = tree_with_value
+
     result = tree.discard(value)
 
     assert result is None
+
+
+@given(strategies.trees_with_totally_ordered_values)
+def test_properties(tree_with_value: Tuple[Tree, Domain]) -> None:
+    tree, value = tree_with_value
+
+    tree.discard(value)
+
+    assert value not in tree
+    assert is_left_subtree_less_than_right_subtree(tree)
 
 
 @given(strategies.empty_trees, strategies.totally_ordered_values)
