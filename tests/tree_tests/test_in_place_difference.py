84cfd74482fa9a99271873a4405509e3c8fb6a46
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-05 19:12:28 +0300

    Complete test for 'TreeBase.__isub__' magic method

--- tests/tree_tests/test_in_place_difference.py
+++ tests/tree_tests/test_in_place_difference.py
@@ -2,7 +2,8 @@ from copy import deepcopy
 
 from hypothesis import given
 
-from tests.utils import TreesPair
+from tests.utils import (TreesPair,
+                         is_left_subtree_less_than_right_subtree)
 from . import strategies
 
 
@@ -14,3 +15,14 @@ def test_connection_with_intersection(trees_pair: TreesPair) -> None:
     left_tree -= right_tree
 
     assert left_tree == original_left_tree - right_tree
+
+
+@given(strategies.trees_pairs)
+def test_properties(trees_pair: TreesPair) -> None:
+    left_tree, right_tree = trees_pair
+
+    left_tree -= right_tree
+
+    assert all(value not in left_tree
+               for value in right_tree)
+    assert is_left_subtree_less_than_right_subtree(left_tree)
