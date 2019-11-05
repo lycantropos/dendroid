2df9451733b49ce04351189d17e312a9480bf622
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-05 19:03:38 +0300

    Complete test for 'TreeBase.clear' method

--- tests/tree_tests/test_clear.py
+++ tests/tree_tests/test_clear.py
@@ -1,6 +1,7 @@
 from hypothesis import given
 
-from tests.utils import Tree
+from tests.utils import (Tree,
+                         is_left_subtree_less_than_right_subtree)
 from . import strategies
 
 
@@ -9,4 +10,11 @@ def test_basic(tree: Tree) -> None:
     result = tree.clear()
 
     assert result is None
+
+
+@given(strategies.trees)
+def test_properties(tree: Tree) -> None:
+    tree.clear()
+
     assert len(tree) == 0
+    assert is_left_subtree_less_than_right_subtree(tree)
