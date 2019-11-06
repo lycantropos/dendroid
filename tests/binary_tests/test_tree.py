1cceb11764bfad41d1410edf8b744c909c194146
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-06 18:16:25 +0300

    Complete test for 'binary.tree' factory

--- tests/binary_tests/test_tree.py
+++ tests/binary_tests/test_tree.py
@@ -1,10 +1,10 @@
 from typing import (List,
-                    Optional)
+                    Optional,
+                    Tuple)
 
 from hypothesis import given
 
-from dendroid.binary import (Tree,
-                             tree)
+from dendroid import binary
 from dendroid.hints import (Domain,
                             SortingKey)
 from tests import strategies
@@ -13,18 +13,24 @@ from tests.utils import (is_left_subtree_less_than_right_subtree,
                          to_height)
 
 
-@given(strategies.totally_ordered_values_lists, strategies.keys)
-def test_basic(values: List[Domain], key: Optional[SortingKey]) -> None:
-    result = tree(*values,
-                  key=key)
+@given(strategies.values_lists_with_keys)
+def test_basic(values_with_key: Tuple[List[Domain], Optional[SortingKey]]
+               ) -> None:
+    values, key = values_with_key
 
-    assert isinstance(result, Tree)
+    result = binary.tree(*values,
+                         key=key)
+
+    assert isinstance(result, binary.Tree)
 
 
-@given(strategies.totally_ordered_values_lists, strategies.keys)
-def test_properties(values: List[Domain], key: Optional[SortingKey]) -> None:
-    result = tree(*values,
-                  key=key)
+@given(strategies.values_lists_with_keys)
+def test_properties(values_with_key: Tuple[List[Domain], Optional[SortingKey]]
+                    ) -> None:
+    values, key = values_with_key
+
+    result = binary.tree(*values,
+                         key=key)
 
     assert len(result) <= len(values)
     assert to_height(result) == log2ceil(len(result))
@@ -35,24 +41,28 @@ def test_properties(values: List[Domain], key: Optional[SortingKey]) -> None:
     assert is_left_subtree_less_than_right_subtree(result)
 
 
-@given(strategies.totally_ordered_values_lists, strategies.keys)
-def test_base_case(values: List[Domain], key: Optional[SortingKey]) -> None:
-    result = tree(key=key)
+@given(strategies.values_lists_with_keys)
+def test_base_case(values_with_key: Tuple[List[Domain], Optional[SortingKey]]
+                   ) -> None:
+    values, key = values_with_key
+
+    result = binary.tree(key=key)
 
     assert len(result) == 0
     assert all(value not in result
                for value in values)
 
 
-@given(strategies.non_empty_totally_ordered_values_lists, strategies.keys)
-def test_step(non_empty_values: List[Domain],
-              key: Optional[SortingKey]) -> None:
-    *values, value = non_empty_values
+@given(strategies.non_empty_values_lists_with_keys)
+def test_properties(values_with_key: Tuple[List[Domain], Optional[SortingKey]]
+                    ) -> None:
+    values, key = values_with_key
+    *values, value = values
 
-    result = tree(*values,
-                  key=key)
-    next_result = tree(*values, value,
-                       key=key)
+    result = binary.tree(*values,
+                         key=key)
+    next_result = binary.tree(*values, value,
+                              key=key)
 
     assert next_result
     assert len(next_result) == len(result) + (value not in values)
