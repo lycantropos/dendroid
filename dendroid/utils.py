d5007a52c70a1468458044bdc906caea365a1a7e
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-08 15:45:33 +0300

    Extract utilities to 'utils' module

--- dendroid/utils.py
+++ dendroid/utils.py
@@ -1,1 +1,20 @@
+import weakref
+from typing import Optional
+
+from .hints import Domain
+
 to_balanced_tree_height = int.bit_length
+
+
+def _maybe_weakref(object_: Optional[Domain]
+                   ) -> Optional[weakref.ReferenceType]:
+    return (object_
+            if object_ is None
+            else weakref.ref(object_))
+
+
+def _dereference_maybe(maybe_reference: Optional[weakref.ref]
+                       ) -> Optional[Domain]:
+    return (maybe_reference
+            if maybe_reference is None
+            else maybe_reference())
