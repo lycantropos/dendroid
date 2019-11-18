5bc375097973b6a9db78cc1d789f736d4a4a0db1
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-18 14:06:14 +0300

    Complete tests for 'red_black.Tree' class

--- tests/red_black_tests/test_popmin.py
+++ tests/red_black_tests/test_popmin.py
@@ -1,10 +1,12 @@
 from hypothesis import given
 
 from dendroid import red_black
+from dendroid.utils import to_balanced_tree_height
 from tests.utils import (are_nodes_parents_to_children,
                          do_paths_to_leaves_have_same_black_nodes_count,
                          do_red_nodes_have_black_children,
-                         is_root_black)
+                         is_root_black,
+                         to_height)
 from . import strategies
 
 
@@ -12,6 +14,7 @@ from . import strategies
 def test_properties(tree: red_black.Tree) -> None:
     tree.popmin()
 
+    assert to_height(tree) <= 2 * to_balanced_tree_height(len(tree) + 1)
     assert are_nodes_parents_to_children(tree)
     assert is_root_black(tree)
     assert do_red_nodes_have_black_children(tree)
--- tests/red_black_tests/test_symmetric_difference.py
+++ tests/red_black_tests/test_symmetric_difference.py
@@ -3,10 +3,12 @@ from typing import Tuple
 from hypothesis import given
 
 from dendroid import red_black
+from dendroid.utils import to_balanced_tree_height
 from tests.utils import (are_nodes_parents_to_children,
                          do_paths_to_leaves_have_same_black_nodes_count,
                          do_red_nodes_have_black_children,
-                         is_root_black)
+                         is_root_black,
+                         to_height)
 from . import strategies
 
 
@@ -16,6 +18,7 @@ def test_properties(trees_pair: Tuple[red_black.Tree, red_black.Tree]) -> None:
 
     result = left_tree ^ right_tree
 
+    assert to_height(result) <= 2 * to_balanced_tree_height(len(result) + 1)
     assert are_nodes_parents_to_children(result)
     assert is_root_black(result)
     assert do_red_nodes_have_black_children(result)
