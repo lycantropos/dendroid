afe1a27834da882f5ad0169dd49acafc9ee6443f
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-20 09:24:46 +0300

    Complete test for 'avl.Tree' class

--- tests/avl_tests/test_popmax.py
+++ tests/avl_tests/test_popmax.py
@@ -2,6 +2,7 @@ from hypothesis import given
 
 from dendroid import avl
 from tests.utils import (are_balance_factors_normalized,
+                         are_nodes_heights_correct,
                          are_nodes_parents_to_children)
 from . import strategies
 
@@ -11,4 +12,5 @@ def test_properties(tree: avl.Tree) -> None:
     tree.popmax()
 
     assert are_nodes_parents_to_children(tree)
+    assert are_nodes_heights_correct(tree)
     assert are_balance_factors_normalized(tree)
