1f224a6cbb3e60e3fafd93247eb919ba5a7b78d3
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-26 15:10:17 +0300

    Add test for 'splay.Tree.max' method

---
+++ tests/splay_tests/test_max.py
@@ -0,0 +1,11 @@
+from hypothesis import given
+
+from tests.utils import Tree
+from . import strategies
+
+
+@given(strategies.non_empty_trees)
+def test_step(tree: Tree) -> None:
+    result = tree.max()
+
+    assert tree._to_key(result) == tree.root.key
