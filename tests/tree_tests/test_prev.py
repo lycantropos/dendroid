279087ff78e2318318aea02bc27f94fa38925af3
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-26 20:07:00 +0300

    Complete test for 'TreeBase.prev' method

--- tests/tree_tests/test_prev.py
+++ tests/tree_tests/test_prev.py
@@ -24,6 +24,14 @@ def test_step(tree_with_value: Tuple[Tree, Domain]) -> None:
             or tree._to_key(tree.prev(value)) < tree._to_key(value))
 
 
+@given(strategies.non_empty_trees_with_external_values)
+def test_external_value(tree_with_value: Tuple[Tree, Domain]) -> None:
+    tree, value = tree_with_value
+
+    with pytest.raises(ValueError):
+        tree.prev(value)
+
+
 @given(strategies.non_empty_trees)
 def test_minimum(tree: Tree) -> None:
     minimum = tree.min()
