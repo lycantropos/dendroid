01615285e6636d3375175ff768ff85fc163b12cf
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-26 20:21:09 +0300

    Add test for 'splay.Tree.prev' method

---
+++ tests/splay_tests/test_prev.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from dendroid.hints import Domain
+from tests.utils import Tree
+from . import strategies
+
+
+@given(strategies.non_empty_trees_with_their_non_min_values)
+def test_properties(tree_with_value: Tuple[Tree, Domain]) -> None:
+    tree, value = tree_with_value
+
+    result = tree.prev(value)
+
+    assert tree._to_key(result) == tree.root.key
