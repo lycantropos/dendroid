ab2436acd9a17fc521ad53d7d3cb26b2c8455adf
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-26 13:44:25 +0300

    Complete test for 'TreeBase.__contains__' method

--- tests/tree_tests/test_contains.py
+++ tests/tree_tests/test_contains.py
@@ -3,14 +3,26 @@ from typing import Tuple
 from hypothesis import given
 
 from dendroid.hints import Domain
-from tests.utils import Tree
+from tests.utils import (Tree,
+                         equivalence,
+                         to_tree_including_value)
 from . import strategies
 
 
-@given(strategies.trees_with_values)
-def test_basic(tree_with_value: Tuple[Tree, Domain]) -> None:
+@given(strategies.empty_trees_with_values)
+def test_base_case(tree_with_value: Tuple[Tree, Domain]) -> None:
     tree, value = tree_with_value
 
-    result = value in tree
+    assert value not in tree
 
-    assert isinstance(result, bool)
+
+@given(strategies.trees_with_values_pairs)
+def test_step(tree_with_values_pair: Tuple[Tree, Tuple[Domain, Domain]]
+              ) -> None:
+    tree, (extra_value, value) = tree_with_values_pair
+
+    next_tree = to_tree_including_value(tree, extra_value)
+
+    assert equivalence(value in next_tree,
+                       value in tree
+                       or tree._to_key(value) == tree._to_key(extra_value))
