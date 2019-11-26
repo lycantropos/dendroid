52c86c41ad644e82db27c8c072b8da716e88fc40
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-26 21:11:00 +0300

    Complete test for 'splay.Tree.next' method

--- tests/splay_tests/test_next.py
+++ tests/splay_tests/test_next.py
@@ -2,13 +2,13 @@ from typing import Tuple
 
 from hypothesis import given
 
+from dendroid import splay
 from dendroid.hints import Domain
-from tests.utils import Tree
 from . import strategies
 
 
 @given(strategies.non_empty_trees_with_their_non_max_values)
-def test_properties(tree_with_value: Tuple[Tree, Domain]) -> None:
+def test_properties(tree_with_value: Tuple[splay.Tree, Domain]) -> None:
     tree, value = tree_with_value
 
     result = tree.next(value)
