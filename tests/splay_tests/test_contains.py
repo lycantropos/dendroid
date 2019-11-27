80daa5c4db2946d02481b1db8e1ab83342cd81da
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-27 14:06:17 +0300

    Complete test for 'splay.Tree.__contains__' magic method

--- tests/splay_tests/test_contains.py
+++ tests/splay_tests/test_contains.py
@@ -4,7 +4,9 @@ from hypothesis import given
 
 from dendroid.hints import Domain
 from tests.utils import (Tree,
-                         implication)
+                         implication,
+                         to_height,
+                         to_max_binary_tree_height)
 from . import strategies
 
 
@@ -13,3 +15,12 @@ def test_properties(tree_with_value: Tuple[Tree, Domain]) -> None:
     tree, value = tree_with_value
 
     assert implication(value in tree, tree.root.key == tree._to_key(value))
+
+
+@given(strategies.non_empty_trees)
+def test_accessing_in_order(tree: Tree) -> None:
+    for element in sorted(tree,
+                          key=tree.key):
+        element in tree
+
+    assert to_height(tree) == to_max_binary_tree_height(tree)
