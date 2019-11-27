9d4c3a1a6d3e190b5c827d2f9f72e0987a082c1d
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-27 15:42:30 +0300

    Complete type annotations for 'splay.Tree' class tests

--- tests/splay_tests/test_contains.py
+++ tests/splay_tests/test_contains.py
@@ -2,23 +2,23 @@ from typing import Tuple
 
 from hypothesis import given
 
+from dendroid import splay
 from dendroid.hints import Domain
-from tests.utils import (Tree,
-                         implication,
+from tests.utils import (implication,
                          to_height,
                          to_max_binary_tree_height)
 from . import strategies
 
 
 @given(strategies.non_empty_trees_with_values)
-def test_properties(tree_with_value: Tuple[Tree, Domain]) -> None:
+def test_properties(tree_with_value: Tuple[splay.Tree, Domain]) -> None:
     tree, value = tree_with_value
 
     assert implication(value in tree, tree.root.key == tree._to_key(value))
 
 
 @given(strategies.non_empty_trees)
-def test_accessing_in_order(tree: Tree) -> None:
+def test_accessing_in_order(tree: splay.Tree) -> None:
     for element in tree:
         element in tree
 
--- tests/splay_tests/test_min.py
+++ tests/splay_tests/test_min.py
@@ -1,11 +1,11 @@
 from hypothesis import given
 
-from tests.utils import Tree
+from dendroid import splay
 from . import strategies
 
 
 @given(strategies.non_empty_trees)
-def test_properties(tree: Tree) -> None:
+def test_properties(tree: splay.Tree) -> None:
     result = tree.min()
 
     assert tree._to_key(result) == tree.root.key
