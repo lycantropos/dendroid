fc69edb3840a848a3f878330f71be16844971149
Azat Ibrakov <azatibrakov@gmail.com>
2019-11-27 14:34:47 +0300

    Add test for 'TreeBase.__reversed__' method

---
+++ tests/tree_tests/test_reversed.py
@@ -0,0 +1,29 @@
+from hypothesis import given
+from lz.iterating import (capacity,
+                          pairwise)
+
+from tests.utils import Tree
+from . import strategies
+
+
+@given(strategies.trees)
+def test_size(tree: Tree) -> None:
+    result = reversed(tree)
+
+    assert capacity(result) == len(tree)
+
+
+@given(strategies.trees)
+def test_elements(tree: Tree) -> None:
+    result = reversed(tree)
+
+    assert all(element in tree
+               for element in result)
+
+
+@given(strategies.trees_with_two_or_more_nodes)
+def test_order(tree: Tree) -> None:
+    result = reversed(tree)
+
+    assert all(tree._to_key(element) > tree._to_key(next_element)
+               for element, next_element in pairwise(result))
