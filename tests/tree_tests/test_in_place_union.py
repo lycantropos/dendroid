edbc22b1d02cdb7f64a167ccfe3070c3519338bf
Azat Ibrakov <azatibrakov@gmail.com>
2019-12-19 19:30:12 +0300

    Complete test for 'TreeBase.__ior__' magic method

--- tests/tree_tests/test_in_place_union.py
+++ tests/tree_tests/test_in_place_union.py
@@ -11,7 +11,7 @@ from . import strategies
 
 
 @given(strategies.trees_pairs)
-def test_connection_with_intersection(trees_pair: TreesPair) -> None:
+def test_connection_with_union(trees_pair: TreesPair) -> None:
     left_tree, right_tree = trees_pair
     original_left_tree = deepcopy(left_tree)
 
