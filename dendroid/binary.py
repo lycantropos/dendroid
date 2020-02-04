73ab2dc23e4cc9a1a508cc3188591d7f0763b26d
Azat Ibrakov <azatibrakov@gmail.com>
2020-02-04 15:28:03 +0300

    Complete 'TreeBase.__lt__' magic method

--- dendroid/binary.py
+++ dendroid/binary.py
@@ -176,10 +176,10 @@ class TreeBase(ABC, Generic[Domain]):
 
     def __lt__(self, other: 'TreeBase[OtherDomain]') -> bool:
         """Checks if the tree is a strict subset of given one."""
-        if not isinstance(other, TreeBase):
-            return NotImplemented
         return (len(self) < len(other)
-                and self <= other and self != other)
+                and self <= other and self != other
+                if isinstance(other, TreeBase)
+                else NotImplemented)
 
     def __gt__(self, other: 'TreeBase[OtherDomain]') -> bool:
         """Checks if the tree is a strict superset of given one."""
