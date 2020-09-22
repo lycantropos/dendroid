3ba75aa5a2aa273c953b4d2e21d5fec52e468fc7
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-22 14:57:33 +0500

    Complete tests for 'red_black' module

--- tests/red_black_tests/test_discard.py
+++ tests/red_black_tests/test_discard.py
@@ -2,21 +2,22 @@ from typing import Tuple
 
 from hypothesis import given
 
-from dendroid import red_black
-from dendroid.hints import Domain
-from tests.utils import (are_nodes_parents_to_children,
+from dendroid.hints import Value
+from tests.utils import (Set,
+                         are_nodes_parents_to_children,
                          do_paths_to_leaves_have_same_black_nodes_count,
                          do_red_nodes_have_black_children,
                          is_root_black)
 from . import strategies
 
 
-@given(strategies.trees_with_values)
-def test_properties(tree_with_value: Tuple[red_black.Tree, Domain]) -> None:
-    tree, value = tree_with_value
+@given(strategies.sets_with_values)
+def test_properties(set_with_value: Tuple[Set, Value]) -> None:
+    set_, value = set_with_value
 
-    tree.discard(value)
+    set_.discard(value)
 
+    tree = set_.tree
     assert are_nodes_parents_to_children(tree)
     assert is_root_black(tree)
     assert do_red_nodes_have_black_children(tree)
--- tests/red_black_tests/test_pop.py
+++ tests/red_black_tests/test_pop.py
@@ -1,17 +1,18 @@
 from hypothesis import given
 
-from dendroid import red_black
-from tests.utils import (are_nodes_parents_to_children,
+from tests.utils import (Set,
+                         are_nodes_parents_to_children,
                          do_paths_to_leaves_have_same_black_nodes_count,
                          do_red_nodes_have_black_children,
                          is_root_black)
 from . import strategies
 
 
-@given(strategies.non_empty_trees)
-def test_properties(tree: red_black.Tree) -> None:
-    tree.pop()
+@given(strategies.non_empty_sets)
+def test_properties(set_: Set) -> None:
+    set_.pop()
 
+    tree = set_.tree
     assert are_nodes_parents_to_children(tree)
     assert is_root_black(tree)
     assert do_red_nodes_have_black_children(tree)
