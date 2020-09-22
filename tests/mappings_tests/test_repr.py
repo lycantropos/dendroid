aea78ae21afab73eab91f51d89eb74da7867beb7
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-23 01:50:18 +0500

    Add test for 'Map.__repr__' magic method

---
+++ tests/mappings_tests/test_repr.py
@@ -0,0 +1,17 @@
+import math
+import sys
+
+from hypothesis import given
+
+from tests.utils import Map
+from . import strategies
+
+
+@given(strategies.maps)
+def test_evaluation(map_: Map) -> None:
+    result = repr(map_)
+
+    type_ = type(map_)
+    # `math` module is required for `inf` object
+    assert eval(result, sys.modules, {**vars(math),
+                                      type_.__qualname__: type_}) == map_
