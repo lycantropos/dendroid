b3189e426f3fc193eeb3c6a11706726cc89fa561
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-23 01:45:24 +0500

    Add test for 'Set.__repr__' magic method

---
+++ tests/sets_tests/test_repr.py
@@ -0,0 +1,26 @@
+import math
+import sys
+
+from hypothesis import given
+
+from tests.utils import Set
+from . import strategies
+
+
+@given(strategies.sets)
+def test_basic(set_: Set) -> None:
+    result = repr(set_)
+
+    type_ = type(set_)
+
+    assert result.startswith(type_.__qualname__)
+
+
+@given(strategies.sets_with_none_orders)
+def test_evaluation(set_: Set) -> None:
+    result = repr(set_)
+
+    type_ = type(set_)
+    # `math` module is required for `inf` object
+    assert eval(result, sys.modules, {**vars(math),
+                                      type_.__qualname__: type_}) == set_
