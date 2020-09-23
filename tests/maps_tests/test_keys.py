b8436cabbc1f1d7ab4f9f58a3edb43aa2a711543
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-23 14:30:03 +0500

    Add test for 'Map.keys' method

---
+++ tests/maps_tests/test_keys.py
@@ -0,0 +1,30 @@
+from collections import abc
+
+from hypothesis import given
+
+from tests.utils import (Map,
+                         are_keys_equal)
+from . import strategies
+
+
+@given(strategies.maps)
+def test_type(map_: Map) -> None:
+    result = map_.keys()
+
+    assert isinstance(result, abc.Set)
+
+
+@given(strategies.maps)
+def test_size(map_: Map) -> None:
+    result = map_.keys()
+
+    assert len(result) == len(map_)
+
+
+@given(strategies.maps)
+def test_elements(map_: Map) -> None:
+    result = map_.keys()
+
+    assert all(any(are_keys_equal(key, candidate) for candidate in map_)
+               for key in result)
+    assert all(key in result for key in map_)
