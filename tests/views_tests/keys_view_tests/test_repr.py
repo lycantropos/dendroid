75ef936bc59f2d682a55fa3ff12d2a6d0af830da
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-23 17:58:18 +0500

    Add test for 'KeysView.__repr__' magic method

---
+++ tests/views_tests/keys_view_tests/test_repr.py
@@ -0,0 +1,17 @@
+import math
+import sys
+
+from hypothesis import given
+
+from tests.utils import KeysView
+from . import strategies
+
+
+@given(strategies.keys_views)
+def test_evaluation(keys_view: KeysView) -> None:
+    result = repr(keys_view)
+
+    type_ = type(keys_view)
+    # `math` module is required for `inf` object
+    assert eval(result, sys.modules,
+                {**vars(math), type_.__qualname__: type_}) == keys_view
