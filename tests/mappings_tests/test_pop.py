a85cb92fb60940d69687f2bff99c04461784a924
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-23 11:27:09 +0500

    Add test for 'Map.pop' method

---
+++ tests/mappings_tests/test_pop.py
@@ -0,0 +1,48 @@
+from copy import copy
+from typing import Tuple
+
+import pytest
+from hypothesis import given
+
+from dendroid.hints import Key
+from tests.utils import (Map,
+                         is_left_subtree_less_than_right_subtree,
+                         to_height,
+                         to_max_binary_tree_height,
+                         to_min_binary_tree_height)
+from . import strategies
+
+
+@given(strategies.non_empty_maps_with_their_keys)
+def test_properties(map_with_key: Tuple[Map, Key]) -> None:
+    map_, key = map_with_key
+
+    result = map_.pop(key)
+
+    tree = map_.tree
+    assert (to_min_binary_tree_height(tree)
+            <= to_height(tree)
+            <= to_max_binary_tree_height(tree))
+    assert result not in map_
+    assert is_left_subtree_less_than_right_subtree(tree)
+
+
+@given(strategies.empty_maps_with_keys)
+def test_base_case(map_with_key: Tuple[Map, Key]) -> None:
+    map_, key = map_with_key
+
+    with pytest.raises(KeyError):
+        map_.pop(key)
+
+
+@given(strategies.non_empty_maps_with_their_keys)
+def test_step(map_with_key: Tuple[Map, Key]) -> None:
+    map_, key = map_with_key
+    original = copy(map_)
+
+    result = map_.pop(key)
+
+    assert key not in map_
+    assert result not in map_.values()
+    assert result in original.values()
+    assert len(map_) == len(original) - 1
