d400ba57e3bdaeb54434cc4ac286b7ef2669f376
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-23 17:33:00 +0500

    Add test for 'KeysView.__eq__' magic method

--- tests/views_tests/keys_view_tests/strategies.py
+++ tests/views_tests/keys_view_tests/strategies.py
@@ -12,20 +12,21 @@ from dendroid import (avl,
 from dendroid.hints import (Item, Key)
 from tests.strategies import (non_empty_values_lists_with_orders,
                               single_values_with_orders,
+                              to_values_lists_with_orders,
                               two_or_more_values_with_orders,
-                              values_lists_with_orders)
+                              values_lists_with_orders,
+                              values_with_orders_strategies)
 from tests.utils import (KeysView,
                          Map,
-                         ValuesListWithOrder)
+                         ValuesListWithOrder, ValuesListsWithOrder)
 
 factories = (strategies.sampled_from([binary.map_, avl.map_, red_black.map_,
                                       splay.map_])
              .map(partial(compose, Map.keys)))
 
 
-def values_lists_with_orders_to_items_lists(values_list_with_order
-                                            : ValuesListWithOrder
-                                            ) -> List[Item]:
+def values_list_with_order_to_items_list(values_list_with_order
+                                         : ValuesListWithOrder) -> List[Item]:
     values_list, order = values_list_with_order
     return ([(value, value) for value in values_list]
             if order is None
@@ -33,13 +34,13 @@ def values_lists_with_orders_to_items_lists(values_list_with_order
 
 
 items_lists = (values_lists_with_orders
-               .map(values_lists_with_orders_to_items_lists))
+               .map(values_list_with_order_to_items_list))
 non_empty_items_lists = (non_empty_values_lists_with_orders
-                         .map(values_lists_with_orders_to_items_lists))
+                         .map(values_list_with_order_to_items_list))
 single_items = (single_values_with_orders
-                .map(values_lists_with_orders_to_items_lists))
+                .map(values_list_with_order_to_items_list))
 two_or_more_items = (two_or_more_values_with_orders
-                     .map(values_lists_with_orders_to_items_lists))
+                     .map(values_list_with_order_to_items_list))
 
 
 def to_keys_view(factory: Callable[..., KeysView],
@@ -73,3 +74,35 @@ def to_keys_view_with_keys_pair(factory: Callable[..., KeysView],
 
 keys_views_with_keys_pairs = strategies.builds(to_keys_view_with_keys_pair,
                                                factories, two_or_more_items)
+
+
+def values_lists_with_order_to_items_lists(values_lists_with_order
+                                           : ValuesListsWithOrder
+                                           ) -> Tuple[List[Item], ...]:
+    *values_lists, order = values_lists_with_order
+    return (tuple([(value, value) for value in values_list]
+                  for values_list in values_lists)
+            if order is None
+            else tuple([(order(value), value) for value in values_list]
+                       for values_list in values_lists))
+
+
+def to_keys_views_tuple(factory: Callable[..., KeysView],
+                        items_lists: List[List[Item]]) -> Tuple[KeysView, ...]:
+    return tuple(factory(*items_list) for items_list in items_lists)
+
+
+keys_views_pairs = strategies.builds(
+        to_keys_views_tuple,
+        factories,
+        (values_with_orders_strategies
+         .flatmap(partial(to_values_lists_with_orders,
+                          sizes=[(0, None)] * 2))
+         .map(values_lists_with_order_to_items_lists)))
+keys_views_triplets = strategies.builds(
+        to_keys_views_tuple,
+        factories,
+        (values_with_orders_strategies
+         .flatmap(partial(to_values_lists_with_orders,
+                          sizes=[(0, None)] * 3))
+         .map(values_lists_with_order_to_items_lists)))
