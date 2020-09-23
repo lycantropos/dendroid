dd78afa8fe0896a613de9b5b3b921e713885b1ae
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-23 18:02:44 +0500

    Add tests for 'KeysView' binary relations

---
+++ tests/views_tests/keys_view_tests/test_is_strict_subset.py
@@ -0,0 +1,37 @@
+from hypothesis import given
+
+from tests.utils import (Set,
+                         SetsPair,
+                         SetsTriplet,
+                         equivalence,
+                         implication)
+from . import strategies
+
+
+@given(strategies.keys_views)
+def test_irreflexivity(keys_view: Set) -> None:
+    assert not keys_view < keys_view
+
+
+@given(strategies.keys_views_pairs)
+def test_asymmetry(keys_views_pair: SetsPair) -> None:
+    first_keys_view, second_keys_view = keys_views_pair
+
+    assert implication(first_keys_view < second_keys_view,
+                       not second_keys_view < first_keys_view)
+
+
+@given(strategies.keys_views_triplets)
+def test_transitivity(keys_views_triplet: SetsTriplet) -> None:
+    first_keys_view, second_keys_view, third_keys_view = keys_views_triplet
+
+    assert implication(first_keys_view < second_keys_view < third_keys_view,
+                       first_keys_view < third_keys_view)
+
+
+@given(strategies.keys_views_pairs)
+def test_connection_with_greater_than(keys_views_pair: SetsPair) -> None:
+    first_keys_view, second_keys_view = keys_views_pair
+
+    assert equivalence(first_keys_view < second_keys_view,
+                       second_keys_view > first_keys_view)
---
+++ tests/views_tests/keys_view_tests/test_isdisjoint.py
@@ -0,0 +1,51 @@
+from copy import copy
+from typing import Tuple
+
+from hypothesis import given
+
+from dendroid.hints import Key
+from tests.utils import (KeysView,
+                         KeysViewsPair,
+                         equivalence,
+                         implication,
+                         to_keys_view_including_key)
+from . import strategies
+
+
+@given(strategies.keys_views_pairs)
+def test_type(keys_views_pair: KeysViewsPair) -> None:
+    first_keys_view, second_keys_view = keys_views_pair
+
+    result = first_keys_view.isdisjoint(second_keys_view)
+
+    assert isinstance(result, bool)
+
+
+@given(strategies.empty_keys_views_with_keys_views)
+def test_base_case(empty_keys_view_with_keys_view: KeysViewsPair) -> None:
+    empty_keys_view, keys_view = empty_keys_view_with_keys_view
+
+    assert empty_keys_view.isdisjoint(keys_view)
+
+
+@given(strategies.keys_views_pairs_with_keys)
+def test_step(two_keys_views_with_value: Tuple[KeysView, KeysView, Key]
+              ) -> None:
+    left_keys_view, right_keys_view, key = two_keys_views_with_value
+
+    next_left_keys_view = to_keys_view_including_key(left_keys_view, key)
+
+    assert implication(not left_keys_view.isdisjoint(right_keys_view),
+                       not next_left_keys_view.isdisjoint(right_keys_view))
+    assert implication(left_keys_view.isdisjoint(right_keys_view),
+                       equivalence(next_left_keys_view
+                                   .isdisjoint(right_keys_view),
+                                   key not in right_keys_view))
+
+
+@given(strategies.keys_views_pairs)
+def test_symmetry(keys_views_pair: KeysViewsPair) -> None:
+    first_keys_view, second_keys_view = keys_views_pair
+
+    assert equivalence(first_keys_view.isdisjoint(second_keys_view),
+                       second_keys_view.isdisjoint(first_keys_view))
