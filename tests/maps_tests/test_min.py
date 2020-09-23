b2c53cde3aebf86f3b8d7cb9a761a723d0d2cf28
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-23 13:43:44 +0500

    Add test for 'Map.max' method

---
+++ tests/maps_tests/test_min.py
@@ -0,0 +1,19 @@
+import pytest
+from hypothesis import given
+
+from tests.utils import Map
+from . import strategies
+
+
+@given(strategies.empty_maps)
+def test_base_case(map_: Map) -> None:
+    with pytest.raises(ValueError):
+        map_.min()
+
+
+@given(strategies.non_empty_maps)
+def test_step(map_: Map) -> None:
+    result = map_.min()
+
+    assert result in map_.values()
+    assert result == min(map_.items())[1]
