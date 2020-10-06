0a21f3a686709a46b8ad53a0095c81a00af58077
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-06 13:04:00 +0300

    Complete test for 'Tree.supremum' method

--- tests/trees_tests/test_supremum.py
+++ tests/trees_tests/test_supremum.py
@@ -15,4 +15,7 @@ def test_properties(tree_with_key: Tuple[Tree, Key]) -> None:
     result = tree.supremum(key)
 
     assert (result is NIL and all(node.key < key for node in tree)
-            or not result.key < key)
+            or not result.key < key and all(not key < node.key
+                                            or node is result
+                                            or result.key < node.key
+                                            for node in tree))
