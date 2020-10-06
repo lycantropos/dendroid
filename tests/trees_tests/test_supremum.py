8dc733e97623dc4381ea58db594d13c862c86bbb
Azat Ibrakov <azatibrakov@gmail.com>
2020-10-06 12:35:04 +0300

    Add test for 'Tree.supremum' method

---
+++ tests/trees_tests/test_supremum.py
@@ -0,0 +1,18 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from dendroid.hints import Key
+from tests.utils import (NIL,
+                         Tree)
+from . import strategies
+
+
+@given(strategies.trees_with_keys)
+def test_properties(tree_with_key: Tuple[Tree, Key]) -> None:
+    tree, key = tree_with_key
+
+    result = tree.supremum(key)
+
+    assert (result is NIL and all(node.key < key for node in tree)
+            or not result.key < key)
