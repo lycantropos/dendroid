9974a9369a9dad9d0157de388452010e70c6a2f5
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-17 13:51:06 +0700

    Complete 'setup.py' script

--- setup.py
+++ setup.py
@@ -7,11 +7,16 @@ import dendroid
 
 project_base_url = 'https://github.com/lycantropos/dendroid/'
 
+
+def read_file(path_string: str) -> str:
+    return Path(path_string).read_text(encoding='utf-8')
+
+
 setup(name=dendroid.__name__,
       packages=find_packages(exclude=('tests', 'tests.*')),
       version=dendroid.__version__,
       description=dendroid.__doc__,
-      long_description=Path('README.md').read_text(encoding='utf-8'),
+      long_description=read_file('README.md'),
       long_description_content_type='text/markdown',
       author='Azat Ibrakov',
       author_email='azatibrakov@gmail.com',
@@ -33,4 +38,4 @@ setup(name=dendroid.__name__,
       url=project_base_url,
       download_url=project_base_url + 'archive/master.zip',
       python_requires='>=3.5',
-      install_requires=Path('requirements.txt').read_text(encoding='utf-8'))
+      install_requires=read_file('requirements.txt'))
