e6da0baab11a3b358a184377963e0aa3decbec3d
Azat Ibrakov <azatibrakov@gmail.com>
2023-03-25 12:02:12 +0100

    Complete type annotations

--- dendroid/binary.py
+++ dendroid/binary.py
@@ -318,6 +318,18 @@ def map_(*items: _Item[_Key, _Value]) -> _Map[_Key, _Value]:
     return _Map(Tree.from_components(*_split_items(items)))
 
 
+@_t.overload
+def set_(*values: _Value,
+         key: None = ...) -> _Set[_Value]:
+    ...
+
+
+@_t.overload
+def set_(*values: _Value,
+         key: _Order[_Value, _Key]) -> _KeyedSet[_Value]:
+    ...
+
+
 def set_(
         *values: _Value,
         key: _t.Optional[_Order[_Value, _Key]] = None
