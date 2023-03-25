9472a882f6cd98e5b0e316e5b01071c780ced3d1
Azat Ibrakov <azatibrakov@gmail.com>
2023-03-25 17:38:17 +0100

    Fix test of 'ItemsView.__contains__' magic method

--- tests/views_tests/items_view_tests/test_contains.py
+++ tests/views_tests/items_view_tests/test_contains.py
@@ -5,6 +5,7 @@ from hypothesis import given
 from dendroid.hints import Item
 from tests.utils import (ItemsView,
                          are_items_equal,
+                         are_items_keys_equal,
                          equivalence,
                          to_items_view_including_item)
 from . import strategies
@@ -18,11 +19,14 @@ def test_base_case(items_view_with_item: Tuple[ItemsView, Item]) -> None:
 
 
 @given(strategies.items_views_with_items_pairs)
-def test_step(items_view_with_items_pair: Tuple[ItemsView, Tuple[Item, Item]]
-              ) -> None:
+def test_step(
+        items_view_with_items_pair: Tuple[ItemsView, Tuple[Item, Item]]
+) -> None:
     items_view, (extra_item, item) = items_view_with_items_pair
 
     next_items_view = to_items_view_including_item(items_view, extra_item)
 
     assert equivalence(item in next_items_view,
-                       item in items_view or are_items_equal(item, extra_item))
+                       (item in items_view
+                        and not are_items_keys_equal(item, extra_item))
+                       or are_items_equal(item, extra_item))
